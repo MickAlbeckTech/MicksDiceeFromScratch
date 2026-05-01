e8a5f88a7de269dc10bcee4cdad5869450eda0ae
Mick Albeck <albeckm2@gmail.com>
2026-05-02 09:53:13 +1000

    Successfully use queryselector in dicee.js to change diceNum1 to new image.

---
+++ dicee.js
@@ -0,0 +1,12 @@
+
+
+
+let firstDiceValue = Math.ceil(6 * Math.random()); //generates random number between from 1 to 6
+
+
+
+let secondDiceValue = Math.ceil(6 * Math.random()); 
+
+
+
+document.querySelector(".diceNum1 img").src="images/dice2.png";
