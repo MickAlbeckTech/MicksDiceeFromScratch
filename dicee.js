aa90ef36a1c2147a8603a3a1c37b6de10959a741
Mick Albeck <albeckm2@gmail.com>
2026-05-02 22:15:57 +1000

    Delete all the old if statements replaced by just 2 lines of code.

--- dicee.js
+++ dicee.js
@@ -14,41 +14,5 @@ document.querySelector(".diceNum1 img").src="images/dice" + firstDiceValue + ".p
 document.querySelector(".diceNum2 img").src="images/dice" + secondDiceValue + ".png";
 
 
-// if (firstDiceValue == 1){
-//     document.querySelector(".diceNum1 img").src="images/dice1.png";
-// }
-// if (firstDiceValue == 2){
-//     document.querySelector(".diceNum1 img").src="images/dice2.png";
-// }
-// if (firstDiceValue == 3){
-//     document.querySelector(".diceNum1 img").src="images/dice3.png";
-// }
-// if (firstDiceValue == 4){
-//     document.querySelector(".diceNum1 img").src="images/dice4.png";
-// }
-// if (firstDiceValue == 5){
-//     document.querySelector(".diceNum1 img").src="images/dice5.png";
-// }
-// if (firstDiceValue == 6){
-//     document.querySelector(".diceNum1 img").src="images/dice6.png";
-// }
 
-// if (secondDiceValue == 1){
-//     document.querySelector(".diceNum2 img").src="images/dice1.png";
-// }
-// if (secondDiceValue == 2){
-//     document.querySelector(".diceNum2 img").src="images/dice2.png";
-// }
-// if (secondDiceValue == 3){
-//     document.querySelector(".diceNum2 img").src="images/dice3.png";
-// }
-// if (secondDiceValue == 4){
-//     document.querySelector(".diceNum2 img").src="images/dice4.png";
-// }
-// if (secondDiceValue == 5){
-//     document.querySelector(".diceNum2 img").src="images/dice5.png";
-// }
-// if (secondDiceValue == 6){
-//     document.querySelector(".diceNum2 img").src="images/dice" + secondDiceValue + ".png";
-// }
 
